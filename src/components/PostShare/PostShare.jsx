b2f6ccef668a5cd0afa8130bd8ab5cfc608f6942
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-13 17:23:16 +0530

    Add Upload Functionality for Photo & Style

--- src/components/PostShare/PostShare.jsx
+++ src/components/PostShare/PostShare.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./PostShare.css";
 import ProfileImage from "../../img/profileImg.jpg";
 import {
@@ -6,9 +6,40 @@ import {
   UilPlayCircle,
   UilLocationPoint,
   UilSchedule,
+  UilTimes,
 } from "@iconscout/react-unicons";
 
 const PostShare = () => {
+  // State to store uploaded image
+  const [image, setImage] = useState(null);
+
+  // Reference to the input element
+  const imageRef = useRef(null);
+
+  // Function to handle image change
+  const onImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      // Get the image
+      let img = event.target.files[0];
+
+      setImage({
+        image: URL.createObjectURL(img),
+      });
+    }
+  };
+
+  // Function to handle option click
+  // Control will go to the input element "ImageUpload" div,
+  // then to onImageChange function
+  const handleOptionClick = () => {
+    imageRef.current.click();
+  };
+
+  // Function to handle close button in preview image section
+  function handleClose() {
+    setImage(null);
+  }
+
   return (
     <div className="PostShare">
       {/*  Profile Image */}
@@ -19,23 +50,55 @@ const PostShare = () => {
         <input type="text" placeholder="What's happening?" />
         {/*  Post Options */}
         <div className="postOptions">
-          <div className="option" style={{ color: "var(--photo)" }}>
+          {/* Photo */}
+          <div
+            className="option"
+            style={{ color: "var(--photo)" }}
+            onClick={handleOptionClick}
+          >
             <UilScenery />
             Photo
           </div>
+          {/* Video */}
           <div className="option" style={{ color: "var(--video)" }}>
             <UilPlayCircle />
             Video
           </div>
+          {/* Location */}
           <div className="option" style={{ color: "var(--location)" }}>
             <UilLocationPoint />
             Location
           </div>
+          {/* Schedule */}
           <div className="option" style={{ color: "var(--schedule)" }}>
             <UilSchedule />
             Schedule
           </div>
+
+          {/* Share Button */}
+          <button className="btn ps-btn">Share</button>
+
+          {/* Image Upload */}
+          <div style={{ display: "none" }}>
+            <input
+              type="file"
+              name="myImage"
+              ref={imageRef}
+              onChange={onImageChange}
+            />
+          </div>
         </div>
+
+        {/* Preview Image (from Image Upload section)*/}
+        {image && (
+          <div className="previewImage">
+            {/* Close Button */}
+            <UilTimes onClick={handleClose} />
+
+            {/* Uploaded Image */}
+            <img src={image.image} alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
