47be878da9acb4c2187d0546ed6045026ef2d5dc
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-14 15:26:47 +0530

    Add nav buttons for Home, Profile, Auth

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
+import React, { useState } from "react";
 import "./App.css";
 import Home from "./pages/home/Home";
 import Profile from "./pages/Profile/Profile";
 import Auth from "./pages/Auth/Auth";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("profile");
+
+  let pageContent;
+  if (currentPage === "home") {
+    pageContent = <Home />;
+  } else if (currentPage === "profile") {
+    pageContent = <Profile />;
+  } else if (currentPage === "auth") {
+    pageContent = <Auth />;
+  }
+
   return (
     <div className="App">
+      {/* Background blur */}
       <div className="blur" style={{ top: "-18%", right: "0" }}></div>
       <div className="blur" style={{ top: "36%", left: "-8rem" }}></div>
-      {/* <Home /> */}
-      <Profile />
-      {/* <Auth /> */}
+
+      {/* Navigation */}
+      <div className="nav-div">
+        <button className="btn" onClick={() => setCurrentPage("home")}>
+          Home
+        </button>
+        <button className="btn" onClick={() => setCurrentPage("profile")}>
+          Profile
+        </button>
+        <button className="btn" onClick={() => setCurrentPage("auth")}>
+          Auth
+        </button>
+      </div>
+
+      {/* Display the selected page */}
+      {pageContent}
     </div>
   );
 }
