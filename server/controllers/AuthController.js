29c8e60fb058131235f2e8f0c63078896bd93cb1
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-05-23 12:17:51 +0530

    Complete Login User Feature

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -28,3 +28,26 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Login User
+
+export const loginUser = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Checking if the user exists & password is correct
+    const user = await UserModel.findOne({ username: username });
+    if (user) {
+      const valid = await bcrypt.compare(password, user.password);
+
+      // Convert the above to ternary operator
+      valid
+        ? res.status(200).json(user) // If password is correct
+        : res.status(400).json({ message: "Wrong password" });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,10 +1,11 @@
 // "/auth" route
 
 import express from "express";
-import { registerUser } from "../controllers/AuthController.js";
+import { loginUser, registerUser } from "../controllers/AuthController.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
